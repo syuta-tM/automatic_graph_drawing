de94aae0d1dfb4d5305001dbc317fb2afdd755d8
syuta-tM <63224909+syuta-tM@users.noreply.github.com>
2021-08-09 22:46:24 +0900

    test

--- automatic_graph_drawing.py
+++ automatic_graph_drawing.py
@@ -8,8 +8,6 @@ import sys
 import shutil
 import re
 
-#test
-
 
 if not os.path.exists('settingData.txt'):
     if not os.path.exists('data'):
@@ -22,43 +20,14 @@ if not os.path.exists('settingData.txt'):
     FILE = [p.name for p in TXTfolder.iterdir() if p.is_file]
     #開始行数と終了行数を指定するテキストファイルが存在しない場合作成する
     #settingData.txtが存在しな状態で実行することで規定フォーマットのテキストファイルが生成されます
-    def settingTXT(stNUM , enNUM , fn):
-        f = open(str(path) + '/settingData.txt' , 'w' , encoding='utf-8', newline='\n')
-        f.write('開始行数指定:' + str(stNUM) + '\n終了行数指定:' + str(enNUM) + '\nExcellのファイル名:' + fn + '\nセルの優先順位:B>F>C>G>D>H>E>I>J>K>L>M'
-        + '\n\n-----------------------------------------\n'
-        + 'SAMPLE\n開始行数指定:' + '22,44,68,27' + '\n終了行数指定:' + '26,56,76,36' + '\n＊行数は1から数え始めます\nExcellのファイル名:' + 'sample' + '\n＊.xlsxを記入する必要はありません\nセルの優先順位:B>F>C>G>D>H>E>I>J>K>L>M'
-        + '\n-----------------------------------------')
-        f.close()
 
-    def btn_click():
-        #ボタン処理
-        start_num = int(st.get())
-        end_num = int(en.get())
-        fn = fl.get()
-        root.destroy()
-        settingTXT(start_num , end_num , fn)
+    f = open(str(path) + '/settingData.txt' , 'w' , encoding='utf-8', newline='\n')
+    f.write('開始行数指定:22\n終了行数指定:37\nExcellのファイル名:test\nセルの優先順位:B>F>C>G>D>H>E>I>J>K>L>M'
+    + '\n\n-----------------------------------------\n'
+    + 'SAMPLE\n開始行数指定:' + '22,44,68,27' + '\n終了行数指定:' + '26,56,76,36' + '\n＊行数は1から数え始めます\nExcellのファイル名:' + 'sample' + '\n＊.xlsxを記入する必要はありません\nセルの優先順位:B>F>C>G>D>H>E>I>J>K>L>M'
+    + '\n-----------------------------------------')
+    f.close()
 
-
-    root = tkinter.Tk()
-    root.geometry('300x200')
-    root.title('データがある行数を指定してください')
-    st_lbl = tkinter.Label(text='開始値')
-    st_lbl.place(x=30, y=50)
-    st = tkinter.Entry(width=20)
-    st.place(x=90, y=50)
-    en_lbl = tkinter.Label(text='終了値')
-    en_lbl.place(x=30, y=90)
-    en = tkinter.Entry(width=20)
-    en.place(x=90, y=90)
-    file_lbl = tkinter.Label(text='ファイル名')
-    file_lbl.place(x=30, y=120)
-    fl = tkinter.Entry(width=20)
-    fl.place(x=90, y=120)
-    file_lbl = tkinter.Label(text='＊.xlsxを入力する必要はありません')
-    file_lbl.place(x=30, y=140)
-    btn = tkinter.Button(root, text='決定', command=btn_click)
-    btn.place(x=140, y=170)
-    root.mainloop()
 
 else:
     path = os.getcwd()
